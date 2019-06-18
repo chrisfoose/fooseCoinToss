08afd722f1871dd88fdd03528c524c7c23a2e26a
chrisfoose <47850167+chrisfoose@users.noreply.github.com>
2019-06-18 13:38:15 -1000

    fooseCoinToss.py

---
+++ fooseCoinToss.py
@@ -0,0 +1,26 @@
+# fooseCoinToss
+# Coin Toss Game
+# Debug exerise Chapter 10 "Automate the Boring Stuff With Python"
+
+
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss!  Enter heads or tails:')
+    guess = input()
+tossInt = random.randint(0, 1) # 0 is tails, 1 is heads
+
+if tossInt == 0:
+    toss = 'tails'
+else:
+    toss = 'heads'
+
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope!  Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope.  You are really bad at this game.')
